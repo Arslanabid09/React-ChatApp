1d1c90bc83a67c11598f72e92419800334eb6719
Arslan Abid <arslancode71@gmail.com>
2024-12-02 16:42:23 +0500

    Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,9 @@ import { useDatabase } from '../Context/DatabaseContext';
 import { ID } from 'appwrite';
 import { toast, ToastContainer } from 'react-toastify';
 import { useAuth } from '../Context/AuthContext';
-import { FaTrash } from 'react-icons/fa';
-
+import {BiTrash} from 'react-icons/bi';
+// importing google fonts
+const googleFont = `@import url('https://fonts.googleapis.com/css2?family=Anton&family=Archivo:ital,wght@0,100..900;1,100..900&family=Bebas+Neue&family=DynaPuff:wght@400..700&family=Faculty+Glyphic&family=Hubot+Sans:ital,wght@0,200..900;1,200..900&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Londrina+Sketch&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Space+Grotesk:wght@300..700&family=Teko:wght@300..700&family=Tilt+Warp&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');`
 const Home = () => {
   const { message, addMessage, getMessages,handleDelete } = useDatabase();
   // getting user from useAuth
@@ -40,14 +41,17 @@ const Home = () => {
 
 
   return (
-    <main className="bg-gradient-to-br from-gray-800 via-slate-900 to-black min-h-screen text-white flex justify-center items-center">
+    <main className="text-white  ">
+      <style>
+        {googleFont}
+      </style>
   <ToastContainer />
-  <div className="w-full sm:w-3/4 lg:w-1/2 my-10 bg-gradient-to-br from-slate-800 via-gray-900 to-gray-800 p-6 rounded-xl shadow-2xl">
+  <div className="w-full mx-auto sm:w-3/4  bg-gradient-to-br from-slate-800 via-gray-900 to-gray-800 p-3 mt-6 rounded-xl">
     {/* Main container */}
-    <div className="relative mb-6">
+    <div className="flex gap-3 items-center  font-semibold bg-gradient-to-r from-gray-700 to-gray-800 text-white placeholder-gray-400 resize-none rounded-lg border-2 border-slate-700 ">
       {/* Text area container */}
       <textarea
-        className="p-4 w-full font-semibold bg-gradient-to-r from-gray-700 to-gray-800 text-white placeholder-gray-400 resize-none rounded-lg border-2 border-slate-700 focus:border-blue-500 focus:ring focus:ring-blue-300 transition"
+        className=" w-full p-4 bg-transparent border-none outline-none resize-none"
         placeholder="Say something amazing..."
         onChange={(e) => setText(e.target.value)}
         value={text}
@@ -55,7 +59,7 @@ const Home = () => {
       {/* Send button */}
       {text.length > 0 && (
         <button
-          className="absolute right-3 top-1/2 -translate-y-1/2 bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-5 rounded-lg shadow-lg transition transform hover:scale-105"
+          className="bg-white text-black px-5 py-2 font-semibold rounded-md mx-3"
           onClick={handleMessages}
         >
           Send
@@ -64,23 +68,26 @@ const Home = () => {
     </div>
 
     {/* Message box */}
-    <div className="my-10 px-5 space-y-6 max-h-[400px] overflow-y-auto bg-slate-800 bg-opacity-80 p-4 rounded-lg shadow-inner">
+    <div className="my-3 px-3 max-h-[400px] overflow-y-auto bg-slate-800  p-2 rounded-lg shadow-inner">
       {message &&
         message.map((msg) => (
           <div key={msg.$id} className={`space-y-3 ${msg.user_id ===user.$id ? 'justify-start':'justify-end'}`}>
-            <div className="flex justify-between items-center">
+            <div className="">
               <h4 className="tracking-wide font-bold text-blue-400">
                 {msg.username || 'Your Name'}
               </h4>
-              <p className="text-gray-400 text-xs">{formatTime(msg.$createdAt) || 'time'}</p>
+              <p className="text-gray-400 text-center text-xs">{formatTime(msg.$createdAt).slice(0,11) || 'time'}</p>
             </div>
-            <div className="flex justify-between items-center">
-  <span className={"bg-gradient-to-r from-gray-100 to-gray-200 text-gray-800 inline-block rounded-xl py-2 px-5 font-medium shadow-lg break-words w-auto max-w-[70%]"}>
+            <div className="flex justify-betwee items-start">
+  <span className={`${msg.user_id ===user.$id ? 'bg-gradient-to-r from-gray-100 to-gray-200 text-gray-800 ':' bg-transparent border border-white text-white'}    rounded-xl py-1 px-5 font-medium shadow-lg break-words w-auto max-w-[70%]`} 
+  style={{fontFamily:"Ubuntu, serif"}}
+  >
     {msg.body}
+    <p className="text-gray-400 mt-2 text-end text-xs">{formatTime(msg.$createdAt).slice(12) || 'time'}</p>
   </span>
   {/* this way everyone can del only their own msg */}
   {msg.user_id === user.$id && (
-      <FaTrash
+      <BiTrash
       onClick={() => handleDelete(msg.$id)}
       className="hover:text-red-600 cursor-pointer duration-300 ml-2"
     />
