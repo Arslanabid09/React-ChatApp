07490a5d4b6758f6c9a37817ee2df93c2af5f6b8
Arslan Abid <arslancode71@gmail.com>
2024-12-20 16:35:42 +0500

    404 error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
@@ -18,6 +18,13 @@ const App = () => {
       </Route>
       <Route path='/' element={<Login/>}/>
       <Route path='/signUp' element={<SignUp/>}/>
+      <Route path={'/*'} element={<>
+      <div className='flex flex-col gap-y-5 justify-center items-center h-screen bg-black text-white'>
+      <h1 className='font-extrabold text-9xl'>4<span className=' top-80 text-8xl'>😓</span>4</h1>
+        <span className='text-xl font-semibold'>page not found</span>
+        <Link to={'/Room'} className='bg-white text-black px-5 py-1 font-semibold'>Go back...</Link>
+      </div>
+        </>}/>
     </Routes>
     </>
   )
